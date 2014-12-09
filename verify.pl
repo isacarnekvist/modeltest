d59c097bae74b02d7615b04602b16e3024f6061f
isacarnekvist <isac.arnekvist@gmail.com>
2014-12-09 13:59:59 +0100

    Done!!!!

--- verify.pl
+++ verify.pl
@@ -21,7 +21,7 @@ verify(Input) :-
 
 % Literals
 check(_, L, S, [], X)           :- member([S, SL], L), member(X, SL).
-check(_, L, S, [], neg(X))      :- \+ check(_, L, S, [], X).
+check(T, L, S, [], neg(X))      :- \+ check(T, L, S, [], X).
 
 % And
 check(T, L, S, [], and(F,G))    :- check(T, L, S, [], F), check(T, L, S, [], G).
@@ -30,10 +30,24 @@ check(T, L, S, [], and(F,G))    :- check(T, L, S, [], F), check(T, L, S, [], G).
 check(T, L, S, [], or(F,G))     :- check(T, L, S, [], F); check(T, L, S, [], G).
 
 % AX
+check(T, L, S, V, ax(X))        :- \+ check(T, L, S, V, ex(neg(X))).
 % EX
+check(T, L, S, _, ex(X))        :- member([S, Ts], T), member(Ns, Ts), check(T, L, Ns, [], X).
+
 % AG
+check(T, L, S, V, ag(X))        :- \+ check(T, L, S, V, ef(neg(X))).
+
 % EG
+check(T, L, S, V, eg(X))        :- \+ check(T, L, S, V, af(neg(X))).
+
+
 % EF
-check(T, L, S, V, ef(X))        :- check(T, L, S, V, X).      % Gäller det i nuvarande noden?
-% check(T, L, S, V, ef(X))      :- check ( i nästa steg samma sak + [S|V] )
+check(T, L, S, _, ef(X))        :- check(T, L, S, [], X).      % Gäller det i nuvarande noden?
+check(T, L, S, V, ef(X))        :- \+ member(S, V), member([S, Ts], T), member(Ns, Ts), check(T, L, Ns, [S|V], ef(X)).
+
 % AF
+check(T, L, S, _, af(X))        :- check(T, L, S, [], X).      % Gäller det i nuvarande noden?
+check(T, L, S, V, af(X))        :- \+ member(S, V), member([S, Ts], T), forall(member(Ns, Ts), check(T, L, Ns, [S|V], af(X))).
+
+
+
